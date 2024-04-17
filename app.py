ad26ff71e9ba074b6ffe61cfaac9006dc1aba1c8
Deathlessgore <deathlessgore18@gmail.com>
2024-04-17 16:45:56 +0700

    First Commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, jsonify, request
+from pymongo import MongoClient
+from datetime import datetime
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    articles = list(db.diary.find({},{'_id':False}))
+    return jsonify({'articles': articles}) #data load berbentuk json
+
+@app.route('/diary', methods=['POST'])
+def save_diary():
+    
+    title_receive = request.form.get('title_give')
+    content_receive = request.form.get('content_give')
+    
+    
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d %H-%M-%S") # format waktu yang telah disempurnakan, jangan pake ":" karna tidak support. lu mau 5 jam nyari error cuma gara gara gitu doang? guasih ogah yaaa 
+    
+    file = request.files["file_give"]
+    extension = file.filename.split('.')[-1]
+    filename = f'static/post-{mytime}.{extension}'
+    file.save(filename)
+    
+    profile = request.files['profile_give']
+    extension = profile.filename.split('.')[-1]
+    profilename = f'static/profile-{mytime}.{extension}' 
+    profile.save(profilename)
+    
+    time = today.strftime("%Y-%m-%d")
+    
+    
+    doc = {
+        'file' : filename,
+        'profile' : profilename,
+        'title' : title_receive,
+        'content': content_receive,
+        'time' : time,
+    }
+    db.diary.insert_one(doc)
+    return jsonify({'msg': 'POST request complete!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
+    
+    
+    
+#from datetime import datetime
+
+#today = datetime.now()
+#date_time = today.strftime("%Y-%m-%d %H:%M:%S")##kapital  letters for month and day, lowercase letters for hours minutes seconds
+#print(date_time)
